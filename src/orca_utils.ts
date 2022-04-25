a88603dad4179b21e893dc2de1a918f381e659f5
Martin Kersner <m.kersner@gmail.com>
2022-04-25 15:37:15 +0900

    feat: Extract address of orca pool token

--- src/orca_utils.ts
+++ src/orca_utils.ts
@@ -1,8 +1,12 @@
-import { Network } from "@orca-so/sdk"
+import { Network, OrcaPoolToken } from "@orca-so/sdk"
 import { TokenListProvider } from '@solana/spl-token-registry'
 
 export async function getAllTokens(network: Network) {
     return await new TokenListProvider().resolve().then((tokens) => {
         return tokens.filterByClusterSlug(network).getList()
     })
+}
+
+export function getTokenAddress(token: OrcaPoolToken): string {
+    return token.mint.toBase58()
 }
