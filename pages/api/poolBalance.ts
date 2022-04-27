493bbdc55df7f5b03057419f77c1f60b73364b52
Martin Kersner <m.kersner@gmail.com>
2022-04-27 16:15:30 +0900

    fix: Remove unused import from `poolBalance`

--- pages/api/poolBalance.ts
+++ pages/api/poolBalance.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next"
 import { PublicKey } from "@solana/web3.js"
 
 import { extractParameter } from "@bisonai-orca/utils"
-import { getWithdrawQuote, getPoolFromTokens, withdrawQuoteToJSON } from "@bisonai-orca/pool";
+import { getWithdrawQuote, getPoolFromTokens } from "@bisonai-orca/pool";
 
 // Arguments
 //   network
