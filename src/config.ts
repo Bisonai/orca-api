5090c7dca512a3b26aa0a1165a7e0dc9238790f5
Martin Kersner <m.kersner@gmail.com>
2022-04-28 11:38:09 +0900

    feat(poolDeposit): Check if account has enough fund to pay fees

--- src/config.ts
+++ src/config.ts
@@ -5,4 +5,5 @@ export const CONFIG = {
     DECIMAL_BASE: 10,
     SWAP_FEE: new Decimal(0.000015),
     WITHDRAW_FEE: new Decimal(0.000015),
+    POOL_DEPOSIT_FEE: new Decimal(0.000015),
 } as const;
