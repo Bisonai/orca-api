84af8b1b0972145a4b43a0f536a41696c71e0a69
Martin Kersner <m.kersner@gmail.com>
2022-04-30 18:16:05 +0900

    feat: Add POST `/farm/deposit`

--- src/farm/farm.controller.ts
+++ src/farm/farm.controller.ts
@@ -2,7 +2,10 @@ import { Controller, Get, Post, Body, HttpCode } from '@nestjs/common';
 import { PublicKey } from '@solana/web3.js';
 import { FarmService } from './farm.service';
 import { getFarms, getFarmFromTokens } from '@bisonai-orca/farm';
+import { getPoolFromTokens } from '@bisonai-orca/pool';
 import { FarmBalanceDto } from './dto/farm-balance.dto';
+import { FarmDepositDto } from './dto/farm-deposit.dto';
+import { keypairFromBs58 } from '@bisonai-orca/solana-utils';
 
 @Controller('farm')
 export class FarmController {
@@ -27,6 +30,30 @@ export class FarmController {
         return await this.farmService.balance({
             farm,
             publicKey,
+        });
+    }
+
+    @Post('deposit')
+    @HttpCode(200)
+    async deposit(@Body() dto: FarmDepositDto) {
+        const pool = getPoolFromTokens(
+            dto.network,
+            dto.tokenA,
+            dto.tokenB,
+        );
+        const farm = getFarmFromTokens(
+            dto.network,
+            dto.tokenA,
+            dto.tokenB,
+            dto.farmType,
+        );
+
+        // FIXME: move outside of the execution REST API
+        const keypair = keypairFromBs58(dto.publicKey, dto.secretKey);
+        return await this.farmService.deposit({
+            farm,
+            pool,
+            keypair,
         });
     }
 }
