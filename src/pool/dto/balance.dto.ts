0f618c74ad652efb5d91ec31e296567d92c6c5a8
Martin Kersner <m.kersner@gmail.com>
2022-04-30 14:48:33 +0900

    feat: Add Swagger UI

--- src/pool/dto/balance.dto.ts
+++ src/pool/dto/balance.dto.ts
@@ -1,15 +1,27 @@
 import { IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { CONFIG } from '@bisonai-orca/config';
 
 export class BalanceDto {
     @IsNotEmpty()
+    @ApiProperty({ enum: CONFIG.ALLOWED_NETWORKS })
     network: string;
 
     @IsNotEmpty()
+    @ApiProperty({
+        example: 'ETH'
+    })
     tokenA: string;
 
     @IsNotEmpty()
+    @ApiProperty({
+        example: 'SOL'
+    })
     tokenB: string;
 
     @IsNotEmpty()
+    @ApiProperty({
+        description: 'Base58 encoded public key'
+    })
     publicKey: string;
 }
--- src/pool/dto/withdraw.dto.ts
+++ src/pool/dto/withdraw.dto.ts
@@ -1,18 +1,33 @@
 import { IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { CONFIG } from '@bisonai-orca/config';
 
 export class WithdrawDto {
     @IsNotEmpty()
+    @ApiProperty({ enum: CONFIG.ALLOWED_NETWORKS })
     network: string;
 
     @IsNotEmpty()
+    @ApiProperty({
+        example: 'ETH'
+    })
     tokenA: string;
 
     @IsNotEmpty()
+    @ApiProperty({
+        example: 'SOL'
+    })
     tokenB: string;
 
     @IsNotEmpty()
+    @ApiProperty({
+        description: 'Base58 encoded public key'
+    })
     publicKey: string;
 
     @IsNotEmpty()
+    @ApiProperty({
+        description: 'Base58 encoded secret key'
+    })
     secretKey: string;
 }
