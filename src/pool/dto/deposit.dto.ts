19d92a595a02c5a4537da6226792453f19a01c8d
Martin Kersner <m.kersner@gmail.com>
2022-04-30 22:21:07 +0900

    fix: Rename pool DTOs by adding `Pool` prefix

--- src/pool/dto/deposit.dto.ts
+++ src/pool/dto/deposit.dto.ts
@@ -2,7 +2,7 @@ import { IsNotEmpty } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { CONFIG } from '@bisonai-orca/config';
 
-export class DepositDto {
+export class PoolDepositDto {
     @IsNotEmpty()
     @ApiProperty({ enum: CONFIG.ALLOWED_NETWORKS })
     network: string;
